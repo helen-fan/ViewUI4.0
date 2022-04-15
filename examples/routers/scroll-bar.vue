12404c0a96f2093143c13f64bbece93fff1b5378
helen-fan <fhl911016@163.com>
2022-04-15 14:55:13 +0800

    first commit

---
+++ examples/routers/scroll-bar.vue
@@ -0,0 +1,33 @@
+<template>
+  <ScrollBar height="400" width="200">
+    <section v-for="item in list">
+        <div class="city">
+            <p>{{ item }}</p>
+        </div>
+    </section>
+  </ScrollBar>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            list: [1,2,3,4,5,6,7,8,9,10]
+        };
+    }
+};
+</script>
+<style>
+    section {
+        margin: 20px;
+    }
+    section > div {
+        width: 300px;
+        padding: 15px;
+        margin: 5px;
+        background-color: #ddf;
+        border: 1px solid #446;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+
+</style>
